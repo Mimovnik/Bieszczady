9127cb4fdc2b82437a99e6ce02bb367e9799cb64
mimovnik <mimovnik@protonmail.com>
2024-06-03 17:43:29 +0200

    feat: generate caves

--- scenes/Terrain.gd
+++ scenes/Terrain.gd
@@ -4,8 +4,34 @@ extends Node2D
 
 var background = 0
 var foreground = 1
+
 var grass_dirt = Vector2i(0, 0)
+var dirt = Vector2i(0, 1)
+
+var width: int = 100
+var height: int = 100
+
+var noise: FastNoiseLite = null
+
+@export
+var frequency: float
+
+@export
+var noise_seed: int
+
+@export
+var cave_threshold: float
 
 func _ready():
-	tile_map.set_cell(foreground, Vector2i(1, 3), 0, grass_dirt)
-	print_debug("elo")
+	generate_noise()
+
+	for y in range(height):
+		for x in range(width):
+			if noise.get_noise_2d(x, y) > cave_threshold:
+				tile_map.set_cell(foreground, Vector2i(x, y), 0, dirt)
+
+func generate_noise():
+	noise = FastNoiseLite.new()
+	noise.noise_type = FastNoiseLite.TYPE_PERLIN
+	noise.frequency = frequency
+	noise.seed = noise_seed
