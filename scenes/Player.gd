8ea68e9401881c06f0a697a53ce4c3479496faad
mimovnik <mimovnik@protonmail.com>
2024-06-03 17:09:58 +0200

    feat: flip player sprite

--- scenes/Player.gd
+++ scenes/Player.gd
@@ -1,5 +1,7 @@
 extends CharacterBody2D
 
+@onready var sprite = $AnimatedSprite2D
+
 const SPEED = 300.0
 
 func _physics_process(_delta):
@@ -14,5 +16,10 @@ func _physics_process(_delta):
 		velocity.y = vertical * SPEED
 	else:
 		velocity.y = move_toward(velocity.y, 0, SPEED)
+
+	if velocity.x > 0:
+		sprite.flip_h = false
+	elif velocity.x < 0:
+		sprite.flip_h = true
 
 	move_and_slide()
