59364564fd9c689dea10efc9ff9cb40e1fba4cf1
mimovnik <mimovnik@protonmail.com>
2024-07-11 20:31:22 +0200

    feat: blocks drop items

--- terrain/Block.gd
+++ terrain/Block.gd
@@ -4,6 +4,8 @@ class_name Block extends Node
 
 @export var durability: int
 
+@export var drop_scene: PackedScene
+
 var coords: Vector2i
 
 var layer: int
@@ -15,4 +17,15 @@ signal durability_depleted(block: Block)
 func hit(damage: int):
 	durability -= damage
 	if durability <= 0:
+		_drop()
 		durability_depleted.emit(self)
+
+func _drop():
+	if drop_scene == null:
+		return
+
+	var drop = drop_scene.instantiate()
+	var terrain = get_parent().get_parent()
+	
+	drop.position = terrain.block_map.map_to_local(coords)
+	terrain.add_child(drop)
